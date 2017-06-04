043bb8dfb6e322c0ee856c7f5da2e5b862c2de54
John Armitage <armitage.john.h@gmail.com>
2017-06-04 19:42:37 +0100

    Followed SmallChange initial changes except for alterations to main.cpp and main.h

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SMC");
-    case mBTC: return QString("mSMC");
-    case uBTC: return QString::fromUtf8("μSMC");
+    case BTC: return QString("RGR");
+    case mBTC: return QString("mRGR");
+    case uBTC: return QString::fromUtf8("μRGR");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SmallChange");
-    case mBTC: return QString("Milli-SmallChange (1 / 1,000)");
-    case uBTC: return QString("Micro-SmallChange (1 / 1,000,000)");
+    case BTC: return QString("RGRcoin");
+    case mBTC: return QString("Milli-RGRcoin (1 / 1,000)");
+    case uBTC: return QString("Micro-RGRcoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
